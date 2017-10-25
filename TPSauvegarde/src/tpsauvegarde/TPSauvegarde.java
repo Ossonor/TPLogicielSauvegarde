883a5bb0eda1b3f88e6e72730498f91e8708bcf2
Mikadaverik <mika_ratel@hotmail.fr>
2017-10-25 14:40:58 +0200

    AES File Encryption
    Chiffre un fichier nomé "document.txt" en AES

---
+++ TPSauvegarde/src/tpsauvegarde/TPSauvegarde.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tpsauvegarde;
+
+import java.io.File;
+
+/**
+ *
+ * @author Mikaël
+ */
+public class TPSauvegarde {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String key = "Mary has one cat";
+        File inputFile = new File("document.txt");
+        File encryptedFile = new File("document.encrypted");
+        File decryptedFile = new File("document.decrypted");
+         
+        try {
+            CryptoUtils.encrypt(key, inputFile, encryptedFile);
+            CryptoUtils.decrypt(key, encryptedFile, decryptedFile);
+        } catch (CryptoException ex) {
+            System.out.println(ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+}
